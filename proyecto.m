4343716828d6a34f79afaa7f0f1769d3cec5d14b
Adolfo Solís <adolfo@Adolfos-MacBook-Pro.local>
2017-12-06 20:50:36 -0600

    reproducir el audio en el match

--- proyecto.m
+++ proyecto.m
@@ -6,7 +6,7 @@ get(micro)
 
 micro = audiorecorder;
 disp('Grabando muestra..')
-recordblocking(micro, 5);
+recordblocking(micro, 90);
 disp('Fin de Grabacion..');
 
 % 1.1 Arreglo de Bits muestra completa
@@ -21,7 +21,7 @@ audiowrite('template1.wav',datos,8000);
 %2. Capturar Sonido Muestra
 
 disp('Grabando prueba..')
-recordblocking(micro, 5);
+recordblocking(micro, 90);
 disp('Fin de Grabacion..');
 
 % 2.1 Arreglo de Bits muestra completa
@@ -34,7 +34,7 @@ audiowrite('template2.wav',datos,8000);
 %3. Capturar sonido test
 
 disp('Grabando prueba..')
-recordblocking(micro, 2);
+recordblocking(micro, 45);
 disp('Fin de Grabacion..');
 
 % 3.1 Arreglo de Bits muestra completa
@@ -68,11 +68,11 @@ if(max(abs(C2)) > max(abs(C1)))
 
     timeDiff = SampleDiff/Fs
 
-
     S1 = alignsignals(S,T2);
     
-    sound(S1)
-
+    playerObj = audioplayer(T2,Fs);
+    play(playerObj,[SampleDiff,length(T2)]);
+    
     figure(3)
     ax(1) = subplot(2,1,1);
     plot(T2)
@@ -93,7 +93,8 @@ else
 
     S1 = alignsignals(S,T1);
     
-    sound(S1)
+    playerObj = audioplayer(T1,Fs);
+    play(playerObj,[SampleDiff,length(T1)]);
 
     figure(4)
     ax(1) = subplot(2,1,1);
